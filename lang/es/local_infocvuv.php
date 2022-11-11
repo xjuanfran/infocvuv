4d10c8902e2b7051328eb75dfb86fd4ea9d12739
ijuanfe <juanfe.ores@gmail.com>
2022-11-11 10:52:37 -0500

    Update plugin documentation

--- lang/es/local_infocvuv.php
+++ lang/es/local_infocvuv.php
@@ -30,83 +30,54 @@ $string['pluginname'] = 'Info CVUV';
 $string['tutorials'] = 'Tutoriales';
 // Tutorials: students.
 $string['students'] = 'Estudiantes';
-$string['students_icon_alt'] = 'Icono de tutoriales para estudiantes';
+$string['student_icon_alt'] = 'Icono de tutoriales para estudiantes';
 $string['student_regulations'] = 'Reglamento estudiantil (Acuerdo 009)';
-$string['student_regulations_url'] = 'http://proxse16.univalle.edu.co/~secretariageneral/consejo-superior/reglamento-estudiantil/acuerdo%20009.pdf';
-$string['student_tutorial'] = 'Tutorial para el uso de la plataforma (autoría CUSE Univalle)';
-$string['student_tutorial_url'] = 'http://cuse.univalle.edu.co/images/TUTORIALES/PDF/Tutorial-para-estudiantes.pdf';
 
 // Tutorials: professors.
 $string['professors'] = 'Profesores';
-$string['professors_icon_alt'] = 'Icono de tutoriales para profesores';
+$string['professor_icon_alt'] = 'Icono de tutoriales para profesores';
 $string['professors_desc'] = 'Tutoriales para el uso de la plataforma:';
 
 // Tutorials: professors - users.
 $string['users'] = 'Usuarios';
 $string['users_enter_cv'] = 'Ingresar al campus virtual';
-$string['users_enter_cv_url'] = 'https://drive.google.com/file/d/13oKJs1S8m4zgxVj_LSlfiEgNlOSbH81_';
 $string['users_restore_password'] = 'Restaurar contraseña';
-$string['users_restore_password_url'] = 'https://drive.google.com/file/d/1TUY0gB9dHxqumPNMGlgmuOM6ceEeX04J';
 $string['users_change_password'] = 'Cambiar contraseña';
-$string['users_change_password_url'] = 'https://drive.google.com/file/d/1P2ZWUmLZTQInv-_ZI9z_wvPflDNdFgOF';
 $string['users_enroll_student'] = 'Matricular estudiante';
-$string['users_enroll_student_url'] = 'https://drive.google.com/file/d/18sR3qUo2sqLYz2ZBOXSjDYwKZiy0k8RX';
 $string['users_unenroll_student'] = 'Desmatricular estudiante';
-$string['users_unenroll_student_url'] = 'https://drive.google.com/file/d/1ZFLOzJKuOpDfqfsCAfMz2q_3WxRFUe7j';
 $string['users_change_user_rol'] = 'Cambiar rol de usuario';
-$string['users_change_user_rol_url'] = 'https://drive.google.com/file/d/1He2t1EaBdMAnas5dUR6bvdzcf0YE3g6j';
 $string['users_create_groups'] = 'Crear grupos y agrupamientos';
-$string['users_create_groups_url'] = 'https://drive.google.com/file/d/1n7KN71PDzA5V8a8kCpIJkrZOKll8epiC';
 $string['users_read_messages'] = 'Leer mensajes';
-$string['users_read_messages_url'] = 'https://drive.google.com/file/d/1i2jFk9Sw3kAhjpFQZYD0zSYGyzaw8x7s';
 
 // Tutorials: professors - courses.
 $string['courses'] = 'Cursos';
 $string['courses_create_course'] = 'Crear curso';
-$string['courses_create_course_url'] = 'https://drive.google.com/file/d/1KTz1k4JALBWtP2gzGRR91qbQ4shiPJTJ';
 $string['courses_restart_course'] = 'Reiniciar curso';
-$string['courses_restart_course_url'] = 'https://drive.google.com/file/d/1jd-ZGue4is-NYaBf-_lRr_a3ugWF7aY_';
 $string['courses_configure_course'] = 'Configurar curso';
-$string['courses_configure_course_url'] = 'https://drive.google.com/file/d/1iL3gvInCPTmc_TYpp8wjHR0x0MsvjTiU';
 $string['courses_add_delete_section'] = 'Adicionar/Eliminar secciones de cursos';
-$string['courses_add_delete_section_url'] = 'https://drive.google.com/file/d/14yYTRAhzT8qGhVSDs1kCLBVT4nW-40Vt';
 $string['courses_free_space'] = 'Liberar espacio';
-$string['courses_free_space_url'] = 'https://drive.google.com/file/d/1u1vDYu5r-rmcDfMS6tyKh_N-QID2ZVgh';
 $string['courses_import_data'] = 'Importar datos';
-$string['courses_import_data_url'] = 'https://drive.google.com/file/d/1YoIWKGzXIoI8TAobgGpjySS4EEMyfaHh';
 $string['courses_tabs_with_restrictions'] = 'Pestañas con restricciones';
-$string['courses_tabs_with_restrictions_url'] = 'https://drive.google.com/file/d/17UQdRNGP6AizbJdfkyGZHecOgSUykELZ';
 $string['courses_export_students_to_excel'] = 'Exportar Listado de Estudiantes a Excel';
-$string['courses_export_students_to_excel_url'] = 'https://drive.google.com/file/d/1TUYilVLOo6xJ4LCRTgSOMeBoO1j1MkCj';
 
 // Tutorials: professors - resources.
 $string['resources_and_activities'] = 'Recursos y actividades';
 $string['resources_concepts'] = 'Recursos y actividades';
-$string['resources_concepts_url'] = 'https://drive.google.com/file/d/1aITf5Nt2dEZLN0txx4Evok4NacKA4ZqF';
 $string['resources_create_assignment'] = 'Crear actividad asignación';
-$string['resources_create_assignment_url'] = 'https://drive.google.com/file/d/1zKj_XqlYSxIjOzGqB_OeEfI4xmYPDLH4';
 $string['resources_assignment_with_restrictions'] = 'Configurar una Asignación con restricciones';
-$string['resources_assignment_with_restrictions_url'] = 'https://drive.google.com/file/d/1EJJp1WPpvqMpCYSFaN2Z54hbB-9YW1Tr';
 $string['resources_file'] = 'Recurso Archivo';
-$string['resources_file_url'] = 'https://drive.google.com/file/d/12C5Uh5VF7FY3eDh7E1EPhH_r1Q165VZe';
 $string['resources_private_files'] = 'Manejo de Archivos';
-$string['resources_private_files_url'] = 'https://drive.google.com/file/d/1pxa8rbTCWT9ee13XdqHtE182l-5eElc0';
 $string['resources_url'] = 'Recurso Enlazar un URL';
-$string['resources_url_url'] = 'https://drive.google.com/file/d/1m-6fzgtlTUBPpB_pquR4eCp3GBa8ni7Z';
 $string['resources_forum'] = 'Actividad Foro';
-$string['resources_forum_url'] = 'https://drive.google.com/file/d/1Irlvq4ZU3lG-QlVXEVX0tPwxcZ8psBAD';
 $string['resources_duplicate'] = 'Duplicar actividades, recursos y pestañas';
-$string['resources_duplicate_url'] = 'https://drive.google.com/file/d/1-qwUtVvxo9vyB10xztG2Sf2_LQq29v3v';
 $string['resources_chat'] = 'Actividad Chat';
-$string['resources_chat_url'] = 'https://drive.google.com/file/d/1bmASn_z51XDli_7tVoEA_ZoEfJ2w3JGm';
 $string['resources_latex'] = 'Integrar LaTex en el Campus Virtual';
-$string['resources_latex_url'] = 'https://drive.google.com/file/d/1DMvTyeBceug8NZsp9W1MmG9Him0FzC-N';
 
 // Tutorials: professors - questionnaires.
 $string['questionnaires'] = 'Cuestionarios';
 $string['questionnaires_questionnaire'] = 'Actividad Cuestionario';
-$string['questionnaires_questionnaire_url'] = 'https://drive.google.com/file/d/1c1yHehHH-Qq6M9Pmi-UZWFegnY2gNvrm';
 $string['questionnaires_question_bank'] = 'Banco de preguntas';
-$string['questionnaires_question_bank_url'] = 'https://drive.google.com/file/d/1HpP5ot3agTSrehPZXU6hAJC5QqzTAAkh';
 $string['questionnaires_add_questions_to_questionnaire'] = 'Asociar preguntas';
-$string['questionnaires_add_questions_to_questionnaire_url'] = 'https://drive.google.com/file/d/1AX3BhbmBs7p8rD4SeQzzlCjfbHliXWyp';
+
+// FAQ.
+$string['faq_icon_alt'] = "Icono de preguntas frecuentes";
