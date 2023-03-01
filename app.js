a44d47198fe98f859f249aa79b46efa9ded701a8
= <juan.garces.leon@correounivalle.edu.co>
2023-03-01 15:18:57 -0500

    Update comments in English and indentation

--- app.js
+++ app.js
@@ -1,42 +1,41 @@
-// Obtener los elementos ul de las listas
+// Get the ul elements from the lists
 const linkList = document.getElementById("link-list");
 const linkList2 = document.getElementById("link-list-2");
+// Create an XMLHttpRequest object to read the text file
 
-// Crear un objeto XMLHttpRequest para leer el archivo de texto
 const xhr = new XMLHttpRequest();
 xhr.open("GET", "links.txt");
 
-// Escuchar el evento onload para leer el contenido del archivo
+// Listen for the onload event to read the file contents
 xhr.onload = function () {
-  // Verificar si la solicitud fue exitosa
+  // Check if the request was successful
   if (xhr.status === 200) {
-    // Dividir el contenido del archivo por salto de línea en un arreglo
+    //Split file content by newline in an array
     const lines = xhr.responseText.split("\n");
-    // Agregar cada línea como un elemento de la lista si es una URL válida
+    // Iterate over the array
     lines.forEach(function (line) {
-      // Verificar si la línea es un enlace válido
       const parts = line.split("|");
       if (parts.length === 3) {
         const name = parts[0].trim();
         const url = parts[1].trim();
         const category = parts[2].trim();
 
-        if (url && category) {
-          // Si la categoría es 'estudiantes', agregar el enlace a la lista de estudiantes
+        if (name && url && category) {
+          //If category is 'students', add the link to the students list
           if (category.toLowerCase() === "students") {
-            const link = document.createElement("li");
             const a = document.createElement("a");
+            const link = document.createElement("li");
             a.href = url;
             a.textContent = name;
             link.appendChild(a);
             linkList.appendChild(link);
           }
-          // Si la categoría es 'cursos', agregar el enlace a la lista de cursos
+          //If category is 'users', add the link to the users list
           else if (category.toLowerCase() === "users") {
             const link = document.createElement("li");
             const a = document.createElement("a");
             a.href = url;
-            a.textContent = " "+ name;
+            a.textContent = " " + name;
             link.appendChild(a);
             linkList2.appendChild(link);
           }
@@ -48,5 +47,5 @@ xhr.onload = function () {
   }
 };
 
-// Enviar la solicitud para leer el archivo de texto
+// Send the request to read the text file
 xhr.send();
